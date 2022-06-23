8d24edde3c6f0bb6c58f4f68eace422bf2f4d7f5
Ahmed <ahmed.ijaz@codingcops.com>
2022-06-23 22:36:39 +0500

    created a model

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_06_21_152839) do
+ActiveRecord::Schema[7.0].define(version: 2022_06_23_172055) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.integer "percent_completed"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
