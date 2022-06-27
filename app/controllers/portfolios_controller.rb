a25fe318b1d4932a7d9c5433c15b4dea93d29344
Ahmed <ahmed.ijaz@codingcops.com>
2022-06-27 22:06:03 +0500

    created add & new functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -4,4 +4,18 @@ class PortfoliosController < ApplicationController
         @portfolio_items = Portfolio.all
     end
 
+    def new
+        @portfolio_item = Portfolio.new
+    end
+
+    def create
+        @portfolio_item = Portfolio.new(params.require(:portfolio).permit(:title, :subtitle))
+
+        respond_to do |format|
+            if @portfolio_item.save()
+                format.html {redirect_to portfolios_path}
+            end
+        end
+    end
+
 end
